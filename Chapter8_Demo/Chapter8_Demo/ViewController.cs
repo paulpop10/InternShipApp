dda4570225fe06527f5f95d18073798bd2881ad9
paulpop10 <paul.pop@fortech.ro>
2019-03-14 09:29:25 +0200

    Chapter 8,TableView Demo App

---
+++ Chapter8_Demo/Chapter8_Demo/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UIKit;
+
+namespace Chapter8_Demo
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            //// Perform any additional setup after loading the view, typically from a nib.
+
+           RestaurantTableView.Source = new MyTableViewDataSource();
+        }
+
+        public override bool PrefersStatusBarHidden()
+        {
+            return true;
+        }
+    }
+}
